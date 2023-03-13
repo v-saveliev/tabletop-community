c639928225e15c3135eca856ee649d86b21c40a9
Vyacheslav Savelev <v.savelev@docrobot.ru>
2023-03-13 08:57:36 +0300

    start

---
+++ src/main/java/org/tabletop/TableTopCommunityApp.java
@@ -0,0 +1,11 @@
+package org.tabletop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class TableTopCommunityApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TableTopCommunityApp.class, args);
+    }
+}
---
+++ src/main/java/org/tabletop/WebConfig.java
@@ -0,0 +1,7 @@
+package org.tabletop;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+}
